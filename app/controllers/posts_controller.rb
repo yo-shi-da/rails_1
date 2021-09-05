f9bd5c472b50e4c038f6ef593c9e23cd7a0c7339
mitsutoshi yoshida <s.d.r.economy@gmail.com>
2021-09-05 20:35:35 +0900

    一覧表示機能

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,12 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -10,4 +14,15 @@ class PostsController < ApplicationController
 
   def edit
   end
+
+  private
+  
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
 end
